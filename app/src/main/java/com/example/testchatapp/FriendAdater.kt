0d26d296d4144a420da79906a31ab00fbc5811af
yashik22 <yashik.mahajan@gmail.com>
2021-08-23 23:35:25 +0530

    Ui, fixed - offline online status

--- app/src/main/java/com/example/testchatapp/FriendAdater.kt
+++ app/src/main/java/com/example/testchatapp/FriendAdater.kt
@@ -1,22 +1,17 @@
 package com.example.testchatapp
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class FriendAdater(val friendList : ArrayList<FriendsDetails>, val listner : (FriendsDetails)->Unit) : RecyclerView.Adapter<FriendAdater.FriendHolder>() {
-
-
+class FriendAdater(val FriendList : ArrayList<FirendsDetails> , val listner : (FirendsDetails)->Unit) : RecyclerView.Adapter<FriendAdater.FriendHolder>() {
     class FriendHolder(view : View) : RecyclerView.ViewHolder(view) {
         val userNameIs = view.findViewById<TextView>(R.id.FriendUserName)
         val userEmail  = view.findViewById<TextView>(R.id.FriendEmail)
-        val onlineImage = view.findViewById<ImageView>(R.id.onlineIv)
-        val offlineImage = view.findViewById<ImageView>(R.id.offlineIv)
-//        val statusFlag = view.findViewById<TextView>(R.id.FriendTvStatus)
-
+        val statusFlag = view.findViewById<TextView>(R.id.FriendTvStatus)
     }
 
     override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : FriendHolder {
@@ -25,26 +20,16 @@ class FriendAdater(val friendList : ArrayList<FriendsDetails>, val listner : (Fr
     }
 
     override fun onBindViewHolder(holder : FriendHolder, position : Int) {
-        val currentItem = friendList[position]
-        holder.onlineImage.visibility = View.INVISIBLE
-        holder.offlineImage.visibility = View.INVISIBLE
-
+        val currentItem = FriendList[position]
         holder.userNameIs.text = currentItem.usernameR
         holder.userEmail.text  = currentItem.emailR
-
-        if(currentItem.status == "Active") {
-            holder.onlineImage.visibility = View.VISIBLE
-        }
-        else{
-            holder.offlineImage.visibility = View.VISIBLE
-        }
-
+        holder.statusFlag.text = currentItem.status
         holder.itemView.setOnClickListener{
             listner(currentItem)
         }
     }
 
     override fun getItemCount() : Int {
-        return friendList.size
+        return FriendList.size
     }
 }
--- app/src/main/java/com/example/testchatapp/FriendFragment.kt
+++ app/src/main/java/com/example/testchatapp/FriendFragment.kt
@@ -22,7 +22,7 @@ class FriendFragment : Fragment() {
     lateinit var recyclerView : RecyclerView
     lateinit var FriendList : ArrayList<FriendsList>
     var friendListIs = mutableListOf<String>()
-    lateinit var connectionList : ArrayList<FriendsDetails>
+    lateinit var connctionList : ArrayList<FirendsDetails>
 
     lateinit var auth : FirebaseAuth
 
@@ -45,7 +45,7 @@ class FriendFragment : Fragment() {
     override fun onViewCreated(view : View, savedInstanceState : Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         FriendList = arrayListOf()
-        connectionList = arrayListOf()
+        connctionList = arrayListOf()
         auth = Firebase.auth
         recyclerView = view.findViewById(R.id.FriendRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
@@ -84,24 +84,24 @@ class FriendFragment : Fragment() {
             override fun onDataChange(snapshot : DataSnapshot) {
 
                 if (snapshot.exists()) {
-                    connectionList.clear()
                     for (friendSnapshot in snapshot.children) {
-                        val friendAre = friendSnapshot.getValue(FriendsDetails::class.java)
+
+                        val friendAre = friendSnapshot.getValue(FirendsDetails::class.java)
                         if (friendAre?.userId in friendListIs) {
-                            connectionList.add(friendAre!!)
+                            connctionList.add(friendAre!!)
 //                            Log.d("FriendList" , "conneciton list $connctionList")
                         }
                     }
-                    fun onItemSelected(friendsDetails : FriendsDetails) {
-                        val bundle = bundleOf("userId" to friendsDetails.userId)
+                    fun onItemSelected(firendsDetails : FirendsDetails) {
+                        val bundle = bundleOf("userId" to firendsDetails.userId)
                         findNavController().navigate(
                             R.id.action_friendFragment_to_chatFragment,
                             bundle
                         )
                     }
 
-                    recyclerView.adapter = FriendAdater(connectionList, ::onItemSelected)
-                    Log.d("FriendList", "conneciton list $connectionList")
+                    recyclerView.adapter = FriendAdater(connctionList, ::onItemSelected)
+                    Log.d("FriendList", "conneciton list $connctionList")
                 }
 
             }
@@ -148,7 +148,7 @@ class FriendFragment : Fragment() {
 data class FriendsList(
     val FriendId : String? = ""
 )
-data class FriendsDetails(
+data class FirendsDetails(
     val userId : String? = "",
     val emailR : String? = "",
     val usernameR : String? = "",
