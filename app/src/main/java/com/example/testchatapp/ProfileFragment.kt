baa4049c1d52ebfb06cd528ff14b7fca138a0854
yashik22 <yashik.mahajan@gmail.com>
2021-08-27 00:37:50 +0530

    26/08/21 Y Changes - user image added in chat, and profile - user can update profile

--- app/src/main/java/com/example/testchatapp/ProfileFragment.kt
+++ app/src/main/java/com/example/testchatapp/ProfileFragment.kt
@@ -1,5 +1,8 @@
 package com.example.testchatapp
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,12 +11,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.navigation.fragment.findNavController
-//import com.bumptech.glide.Glide
+import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 
 class ProfileFragment : Fragment() {
     // For Edit Text Display
@@ -23,7 +27,12 @@ class ProfileFragment : Fragment() {
     lateinit var userProfileUpdateFloatingActionButton: FloatingActionButton
     lateinit var userNameEditFloatingActionButton: FloatingActionButton
     lateinit var userNameUpdateFloatingActionButton: FloatingActionButton
+    lateinit var imageUploadProgressBar: ProgressBar
 
+    //user image
+    lateinit var uri: Uri
+    var imageUrl: String = ""
+    var imageName: String = ""
 
     lateinit var logOut: Button
 
@@ -49,14 +58,54 @@ class ProfileFragment : Fragment() {
         // For Edit Text Display
         userNameEditText = view.findViewById(R.id.userNameEt)
         passwordEditText = view.findViewById(R.id.passTv)
-        imageView = view.findViewById(R.id.imageView)
+        imageView = view.findViewById(R.id.profileIv)
         userProfileUpdateFloatingActionButton = view.findViewById(R.id.userImageFab)
         userNameEditFloatingActionButton = view.findViewById(R.id.editUserNameFab)
         userNameUpdateFloatingActionButton = view.findViewById(R.id.updateUserNameFab)
+        imageUploadProgressBar = view.findViewById(R.id.imageUploadProgress)
 
+        imageUploadProgressBar.visibility = View.INVISIBLE
         userNameUpdateFloatingActionButton.visibility = View.INVISIBLE
 
-        viewProfile()
+        userProfileUpdateFloatingActionButton.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, 0)
+            }
+        }
+
+        val userReference = FirebaseDatabase.getInstance().getReference("Users")
+            .child(auth.currentUser?.uid.toString())
+        userReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                //EditText
+                val userName = snapshot.child("usernameR").value.toString()
+                val photoUrl = snapshot.child("userProfileImgUrl").value.toString()
+                if(snapshot.child("userProfileImgUrl").exists()) {
+                    if (photoUrl.isNotEmpty()) {
+                        Log.d("ProfileFragment", "ImageUrl : $photoUrl")
+                        context?.let { Glide.with(it).load(photoUrl).into(imageView) }
+                    }
+                    else{
+                        context?.let { Glide.with(it).load(R.drawable.image).into(imageView) }
+                    }
+                }
+                else{
+                    context?.let {
+                        Glide.with(it).load(R.drawable.image).into(imageView)
+                    }
+                }
+
+                userNameEditText.isEnabled = false
+                userNameEditText.setText(userName)
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
 
         logOut.setOnClickListener {
             val lastUser = auth.currentUser?.uid
@@ -79,34 +128,47 @@ class ProfileFragment : Fragment() {
             userNameEditFloatingActionButton.visibility = View.VISIBLE
 
             val newUserName = userNameEditText.text.toString()
-            FirebaseDatabase.getInstance().getReference("Users").child(auth.currentUser?.uid.toString()).child("usernameR").setValue(newUserName)
+            FirebaseDatabase.getInstance().getReference("Users")
+                .child(auth.currentUser?.uid.toString()).child("usernameR").setValue(newUserName)
         }
     }
 
-    private fun viewProfile() {
-        val userReference = FirebaseDatabase.getInstance().getReference("Users").child(auth.currentUser?.uid.toString())
-        userReference.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Toast.makeText(context, "Your Image is being Uploaded", Toast.LENGTH_LONG).show()
+        imageUploadProgressBar.visibility = View.VISIBLE
 
-                //EditText
-                val userName = snapshot.child("usernameR").value.toString()
-//                val photoUrl = snapshot.child("profileImageUrl").value.toString()
-//                if(photoUrl.isEmpty()) {
-//                    }
-//                else{
-//                    Log.d("ProfileFragment", "ImageUrl : $photoUrl")
-//                    context?.let {
-//                        Glide.with(it).load(photoUrl).into(imageView)
-//                    }
-//                }
-                userNameEditText.isEnabled = false
-                userNameEditText.setText(userName)
+        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
+            uri = data?.data!! //this data refer to data of our intent
 
-            }
+            uploadImage(uri)
+            imageView.setImageURI(uri)
+        }
+    }
 
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-        })
+    private fun uploadImage(uri: Uri?) {
+        imageName =
+            uri?.lastPathSegment?.removePrefix("raw:/storage/emulated/0/Download/").toString()
+        Log.d(
+            "ProfileFragment",
+            "Image name to be uploaded in firebase storage database : $imageName"
+        )
+        val storageReference = FirebaseStorage.getInstance().reference.child("image/$imageName")
+        if (uri != null) {
+            storageReference.putFile(uri)
+                .addOnSuccessListener { it ->
+                    it.metadata?.reference?.downloadUrl?.addOnSuccessListener {
+                        imageUrl = it.toString()
+                        Log.d("ProfileFragment", "ImageUrl generated: $imageUrl")
+                        FirebaseDatabase.getInstance().getReference("Users")
+                            .child(auth.currentUser?.uid.toString()).child("userProfileImgUrl").setValue(imageUrl)
+                    }
+                    imageUploadProgressBar.visibility = View.INVISIBLE
+                    Toast.makeText(context, "Your Image has been Updated", Toast.LENGTH_LONG).show()
+                }.addOnFailureListener {
+                    imageUploadProgressBar.visibility = View.INVISIBLE
+                    Toast.makeText(context, "Image Upload failed", Toast.LENGTH_LONG).show()
+                }
+        }
     }
 }
--- app/src/main/java/com/example/testchatapp/chatAdapter.kt
+++ app/src/main/java/com/example/testchatapp/chatAdapter.kt
@@ -1,33 +1,39 @@
 package com.example.testchatapp
 
-import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import de.hdodenhof.circleimageview.CircleImageView
 
 class chatAdapter(val chatList : ArrayList<chatDataClass>) : RecyclerView.Adapter<chatAdapter.ChatHolder>() {
 
     private val MESSAGE_TYPE_LEFT = 0
     private val MESSAGE_TYPE_RIGHT = 1
+    lateinit var auth: FirebaseAuth
+    var userImageUrl =""
 
     var firebaseUser : FirebaseUser? = null
 
     class ChatHolder(view : View):RecyclerView.ViewHolder(view) {
         val message = view.findViewById<TextView>(R.id.tvMessage)
+        val userImageView = view.findViewById<CircleImageView>(R.id.userImage)
     }
 
     override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : ChatHolder {
-        if(viewType == MESSAGE_TYPE_RIGHT){
+        auth = Firebase.auth
+            if(viewType == MESSAGE_TYPE_RIGHT){
             val view = LayoutInflater.from(parent.context).inflate(R.layout.item_right , parent ,false)
             return ChatHolder(view)
         }else{
@@ -38,8 +44,18 @@ class chatAdapter(val chatList : ArrayList<chatDataClass>) : RecyclerView.Adapte
 
     override fun onBindViewHolder(holder : ChatHolder, position : Int) {
         val currentItem  = chatList[position]
-        holder.message.text = currentItem.message
+        val userImageRef = FirebaseDatabase.getInstance().getReference("Users").child(auth.currentUser?.uid.toString())
+        userImageRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userImageUrl = snapshot.child("userProfileImgUrl").value.toString()
+                holder.userImageView?.let { Glide.with(it).load(userImageUrl).into(holder.userImageView) }
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+        holder.message.text = currentItem.message
     }
 
     override fun getItemCount() : Int {
